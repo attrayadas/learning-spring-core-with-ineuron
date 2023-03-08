f0d6b59211830d5d3e75ada908d65ad31efdff73
Attraya Das <attrayaghoshdas@gmail.com>
2023-03-08 21:48:00 +0530

    Add IOCProj4-DependencyInjection

---
+++ IOCProj4-DependencyInjection/src/in/ineuron/bean/Person.java
@@ -0,0 +1,51 @@
+package in.ineuron.bean;
+
+public class Person {
+	private String pname;
+	private Integer pid;
+	private String paddress;
+	private float income;
+
+	static {
+		System.out.println("Person.class is loading...");
+		System.out.println("**************************************");
+	}
+
+	public Person() {
+		System.out.println("Person object is instantiated using zero argument constuctor");
+	}
+
+	public void setPname(String pname) {
+		System.out.println("Person.setPname()");
+		this.pname = pname;
+	}
+
+	public void setPid(Integer pid) {
+		System.out.println("Person.setPid()");
+		this.pid = pid;
+	}
+
+	public void setPaddress(String paddress) {
+		System.out.println("Person.setPaddress()");
+		this.paddress = paddress;
+	}
+
+	public void setIncome(float income) {
+		System.out.println("Person.setIncome()");
+		this.income = income;
+	}
+
+	public Person(String pname, Integer pid, String paddress, float income) {
+		System.out.println("Person object is created by Constructor injection");
+		this.pname = pname;
+		this.pid = pid;
+		this.paddress = paddress;
+		this.income = income;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [pname=" + pname + ", pid=" + pid + ", paddress=" + paddress + ", income=" + income + "]";
+	}
+
+}
