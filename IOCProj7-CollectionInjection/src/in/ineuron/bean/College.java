b7db8a45c43432fabd5ee56ca5bb51dc1944fca2
Attraya Das <attrayaghoshdas@gmail.com>
2023-03-12 20:09:58 +0530

    Add IOCProj7-CollectionInjection

---
+++ IOCProj7-CollectionInjection/src/in/ineuron/bean/College.java
@@ -0,0 +1,35 @@
+package in.ineuron.bean;
+
+import java.util.Date;
+import java.util.List;
+
+public class College {
+
+	private List<String> nameList;
+	private List<Date> dateList;
+
+	static {
+		System.out.println("College.class is loading...");
+	}
+
+	public College() {
+		System.out.println("College object is instantiated...");
+	}
+
+	public void setNameList(List<String> nameList) {
+		System.out.println("College.setNameList()");
+		this.nameList = nameList;
+		System.out.println(nameList.getClass()); // class java.util.ArrayList
+	}
+
+	public void setDateList(List<Date> dateList) {
+		System.out.println("College.setDateList()");
+		this.dateList = dateList;
+	}
+
+	@Override
+	public String toString() {
+		return "College [nameList=" + nameList + ", dateList=" + dateList + "]";
+	}
+
+}
---
+++ IOCProj7-CollectionInjection/src/in/ineuron/bean/MarksInfo.java
@@ -0,0 +1,33 @@
+package in.ineuron.bean;
+
+import java.util.Arrays;
+import java.util.Date;
+
+public class MarksInfo {
+	private int marks[];
+	private Date dates[];
+
+	static {
+		System.out.println("MarksInfo.class file is loading...");
+	}
+
+	public MarksInfo() {
+		System.out.println("Object created using zero arg constructor...");
+	}
+
+	public void setMarks(int[] marks) {
+		System.out.println("MarksInfo.setMarks()");
+		this.marks = marks;
+	}
+
+	public void setDates(Date[] dates) {
+		System.out.println("MarksInfo.setDates()");
+		this.dates = dates;
+	}
+
+	@Override
+	public String toString() {
+		return "MarksInfo [marks=" + Arrays.toString(marks) + ", dates=" + Arrays.toString(dates) + "]";
+	}
+
+}
